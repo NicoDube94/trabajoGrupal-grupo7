66b212bee97c0bdcc4e3587686edacbf3c0c1ae3
Hernan57 <Hernan57@DESKTOP-A86A3ST>
2020-10-17 17:59:20 -0300

    TP 6

---
+++ Universidad/src/universidad/Universidad.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidad;
+
+import entidades.Alumno;
+import entidades.Materia;
+import java.time.LocalDate;
+import java.time.Month;
+
+/**
+ *
+ * @author Hernan57
+ */
+public class Universidad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        conexion con = new conexion();
+        Materia laboratorio = new Materia("Laboratorio");
+        MateriaData md = new MateriaData(con);
+        md.guardarMateria(laboratorio);
+        Alumno jose = new Alumno("Jose",LocalDate.of(14, Month.MARCH, 1998),true);
+        AlumnoData ad = new AlumnoData(con);
+        ad.guardarAlumno(jose);
+        
+    }
+    
+}
